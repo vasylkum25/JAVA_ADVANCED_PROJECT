3b7cf3a06b3e05e56decdae08e59ad5c164f94dd
Василь <@192.168.0.103>
2017-08-01 21:22:43 +0300

    Create View Class CafeIndexView

---
+++ JAVA_ADVANCED_PROJECT/src/main/java/kum/model/view/CafeIndexView.java
@@ -0,0 +1,24 @@
+package kum.model.view;
+
+import java.math.BigDecimal;
+
+public class CafeIndexView {
+	
+	private Integer id;
+	
+	private BigDecimal rate;
+	
+	private String name;
+	
+	private String photoUrl;
+	
+	private int version;
+	
+	private String address;
+	
+	private String shortDescription;
+	
+	private String type;
+	
+
+}
