5b6efe67cfc54d25e9d5cc6ff8ed667e996689b8
Василь <@192.168.0.103>
2017-08-01 21:24:43 +0300

    Add Geters and Seters for CafeIndexView

--- JAVA_ADVANCED_PROJECT/src/main/java/kum/model/view/CafeIndexView.java
+++ JAVA_ADVANCED_PROJECT/src/main/java/kum/model/view/CafeIndexView.java
@@ -19,6 +19,71 @@ public class CafeIndexView {
 	private String shortDescription;
 	
 	private String type;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public BigDecimal getRate() {
+		return rate;
+	}
+
+	public void setRate(BigDecimal rate) {
+		this.rate = rate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
 	
 
 }
