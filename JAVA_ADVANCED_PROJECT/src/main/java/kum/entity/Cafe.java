8b7a804a5a29ad520627f858031d5243787310e9
Василь <@192.168.0.103>
2017-08-01 21:55:11 +0300

    Generate Getters and Setters

--- JAVA_ADVANCED_PROJECT/src/main/java/kum/entity/Cafe.java
+++ JAVA_ADVANCED_PROJECT/src/main/java/kum/entity/Cafe.java
@@ -27,6 +27,84 @@ private Integer id;
 	
 	private String photoUrl;
 	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public BigDecimal getRate() {
+		return rate;
+	}
+	public void setRate(BigDecimal rate) {
+		this.rate = rate;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
+	}
+	public int getVersion() {
+		return version;
+	}
+	public void setVersion(int version) {
+		this.version = version;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+	public String getFullDescription() {
+		return fullDescription;
+	}
+	public void setFullDescription(String fullDescription) {
+		this.fullDescription = fullDescription;
+	}
+	public Type getType() {
+		return type;
+	}
+	public void setType(Type type) {
+		this.type = type;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public OpenClose getOpen() {
+		return open;
+	}
+	public void setOpen(OpenClose open) {
+		this.open = open;
+	}
+	public OpenClose getClose() {
+		return close;
+	}
+	public void setClose(OpenClose close) {
+		this.close = close;
+	}
 	private int version;
 	
 	private String address;
--- JAVA_ADVANCED_PROJECT/src/main/java/kum/entity/OpenClose.java
+++ JAVA_ADVANCED_PROJECT/src/main/java/kum/entity/OpenClose.java
@@ -16,4 +16,20 @@ public class OpenClose {
 	private Integer id;
 	
 	private String time;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
 }
