2cc3053cf251ce5679df1cfda7859c7b6454424b
Василь <@192.168.0.103>
2017-08-01 22:41:36 +0300

    Add (fetch = FetchType.LAZY)

--- JAVA_ADVANCED_PROJECT/src/main/java/kum/entity/Cafe.java
+++ JAVA_ADVANCED_PROJECT/src/main/java/kum/entity/Cafe.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -44,9 +45,9 @@ private Integer id;
 	
 	private String email;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	private OpenClose open;
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	private OpenClose close;
 	
 	public Integer getId() {
