9f3ed0abe0e9cb72b86e756f5784e4b54db182d9
Василь <@192.168.0.103>
2017-08-01 22:53:22 +0300

    Create EntityManager and Transaction

--- JAVA_ADVANCED_PROJECT/src/main/java/kum/controler/Main.java
+++ JAVA_ADVANCED_PROJECT/src/main/java/kum/controler/Main.java
@@ -1,5 +1,6 @@
 package kum.controler;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -7,7 +8,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("primary");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
 		
+		
+		em.getTransaction().commit();
+		em.close();
 		factory.close();
 
 	}
