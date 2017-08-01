2eef2d752e3b5c0b6739b74c0b3a167f1ba402d3
Василь <@192.168.0.103>
2017-08-01 21:33:51 +0300

    Create Entity Class Cafe for database

---
+++ JAVA_ADVANCED_PROJECT/src/main/java/kum/entity/Cafe.java
@@ -0,0 +1,33 @@
+package kum.entity;
+
+import java.math.BigDecimal;
+
+public class Cafe {
+	
+private Integer id;
+	
+	private BigDecimal rate;
+	
+	private String name;
+	
+	private String photoUrl;
+	
+	private int version;
+	
+	private String address;
+	
+	private String shortDescription;
+	
+	private String fullDescription;
+	
+	private String type;
+	
+	private String phone;
+	
+	private String email;
+	
+	private String open;
+	
+	private String close;
+
+}
