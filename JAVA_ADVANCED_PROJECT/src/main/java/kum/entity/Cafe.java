431a7f14496c43c3715a0b24fdd344be7a19702c
Василь <@192.168.0.103>
2017-08-01 21:53:46 +0300

    Add anotation ManyToOne

--- JAVA_ADVANCED_PROJECT/src/main/java/kum/entity/Cafe.java
+++ JAVA_ADVANCED_PROJECT/src/main/java/kum/entity/Cafe.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 
@@ -43,8 +44,9 @@ private Integer id;
 	
 	private String email;
 	
+	@ManyToOne
 	private OpenClose open;
-	
+	@ManyToOne
 	private OpenClose close;
 
 }
