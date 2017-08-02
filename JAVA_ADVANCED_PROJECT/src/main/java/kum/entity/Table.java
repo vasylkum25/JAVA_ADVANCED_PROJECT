944f3a89977e987ba18646589a57cd61c4258637
Василь <@192.168.0.105>
2017-08-03 00:29:59 +0300

    Create Table and relation

---
+++ JAVA_ADVANCED_PROJECT/src/main/java/kum/entity/Table.java
@@ -0,0 +1,43 @@
+package kum.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+@javax.persistence.Table(name="_table")
+public class Table extends AbstractEntity{
+
+	private int countOfPeople;
+	
+	private boolean isFree;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Cafe cafe;
+
+	public int getCountOfPeople() {
+		return countOfPeople;
+	}
+
+	public void setCountOfPeople(int countOfPeople) {
+		this.countOfPeople = countOfPeople;
+	}
+
+	public boolean isFree() {
+		return isFree;
+	}
+
+	public void setFree(boolean isFree) {
+		this.isFree = isFree;
+	}
+
+	public Cafe getCafe() {
+		return cafe;
+	}
+
+	public void setCafe(Cafe cafe) {
+		this.cafe = cafe;
+	}
+	
+}
