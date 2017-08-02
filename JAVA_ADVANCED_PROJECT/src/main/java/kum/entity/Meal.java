1da840de9d7f1fc5db1b6aae7fea8e073a477a55
Василь <@192.168.0.105>
2017-08-03 00:14:45 +0300

    AddIngredients and relation

--- JAVA_ADVANCED_PROJECT/src/main/java/kum/entity/Meal.java
+++ JAVA_ADVANCED_PROJECT/src/main/java/kum/entity/Meal.java
@@ -30,6 +30,71 @@ public class Meal extends AbstractEntity {
 	
 	private int weight;
 	
+	@ManyToMany(fetch=FetchType.LAZY)
 	private List<Ingredient> ingredients = new ArrayList<>();
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public Cuisine getCuisine() {
+		return cuisine;
+	}
+
+	public void setCuisine(Cuisine cuisine) {
+		this.cuisine = cuisine;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public List<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(List<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
 	
 }
