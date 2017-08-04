a2b0bbda24e5ddd6afc718a9ea9297ad9a64937c
Василь <@192.168.0.100>
2017-08-04 11:52:39 +0300

    Create consol menu 1

--- JAVA_ADVANCED_PROJECT/src/main/java/kum/controler/Aplication.java
+++ JAVA_ADVANCED_PROJECT/src/main/java/kum/controler/Aplication.java
@@ -9,66 +9,55 @@ import javax.persistence.Persistence;
 
 import kum.entity.Cafe;
 import kum.entity.OpenClose;
+import kum.entity.Type;
 
 public class Aplication {
-	
+
 	EntityManagerFactory factory = Persistence.createEntityManagerFactory("primary");
 	Scanner sc = new Scanner(System.in);
-	
-	public void run(){
-		boolean isRun=true;
-		while(isRun){
-		System.out.println("1. Select Entity of time;");
-		System.out.println("2. Add time;");
-		System.out.println("3. Selekt entity ");
-	switch (sc.next()) {
-	case "1":
-		selectEntity(factory, sc);
-		break;
-	case "2":
-		addTimeToCafe(factory, sc);
-		break;
-	case "3":
-		selectCafe(factory, sc);
-		break;
 
-	default:
-		break;
+	public void run() {
+		boolean isRun = true;
+		while (isRun) {
+			System.out.println("1. Select Entity of time;");
+			System.out.println("2. Selekt entity;");
+			System.out.println("3. Update Cafe;");
+			System.out.println("4. Delet Cafe for id;");
+			switch (sc.next()) {
+			case "1":
+				selectEntity(factory, sc);
+				break;
+			case "2":
+				selectCafe(factory, sc);
+				break;
+			case "3":
+				updaitCafe(factory, sc);
+				break;
+			case "4":
+				removeCafe(factory, sc);
+				break;
+
+			default:
+				break;
+			}
+		}
 	}
-	}
-	}
-	
-	public void selectEntity(EntityManagerFactory factory, Scanner sc){
+
+	public void selectEntity(EntityManagerFactory factory, Scanner sc) {
 		EntityManager em = factory.createEntityManager();
 		em.getTransaction().begin();
 		OpenClose open = new OpenClose();
 		System.out.println("Enter time to open houer and minutes");
-		int oh= sc.nextInt();
+		int oh = sc.nextInt();
 		int om = sc.nextInt();
 		open.setTime(LocalTime.of(oh, om));
 		em.persist(open);
 		em.getTransaction().commit();
 		em.close();
-		
+
 	}
-	
-	
-	public void addTimeToCafe(EntityManagerFactory factory, Scanner sc){
-		EntityManager em = factory.createEntityManager();
-		em.getTransaction().begin();
-		System.out.println("Enter id of time");
-		int id = sc.nextInt();
-		Cafe cafe = new Cafe();
-		OpenClose open = em.find(OpenClose.class, id);
-		cafe.setOpen(open);
-		em.persist(cafe);
-		
-	
-	em.getTransaction().commit();
-	em.close();
-	}
-	
-	public void selectCafe(EntityManagerFactory factory, Scanner sc){
+
+	public void selectCafe(EntityManagerFactory factory, Scanner sc) {
 		EntityManager em = factory.createEntityManager();
 		em.getTransaction().begin();
 		System.out.println("Enter address");
@@ -88,6 +77,31 @@ public class Aplication {
 		OpenClose open = em.find(OpenClose.class, ot);
 		OpenClose close = em.find(OpenClose.class, ct);
 		Cafe cafe = new Cafe();
+		System.out.println("Choos type of cafe: ");
+		System.out.println("1. PUB;");
+		System.out.println("2. SUSHY;");
+		System.out.println("3. BAR;");
+		System.out.println("4. CAFE;");
+		System.out.println("5. RESTAURANT;");
+		switch (sc.next()) {
+		case "1":
+			cafe.setType(Type.PUB);
+			break;
+		case "2":
+			cafe.setType(Type.SUSHY);
+			break;
+		case "3":
+			cafe.setType(Type.BAR);
+			break;
+		case "4":
+			cafe.setType(Type.CAFE);
+			break;
+		case "5":
+			cafe.setType(Type.RESTAURANT);
+			break;
+		default:
+			break;
+		}
 		cafe.setOpen(open);
 		cafe.setClose(close);
 		cafe.setAddress(address);
@@ -96,13 +110,132 @@ public class Aplication {
 		cafe.setName(name);
 		cafe.setPhone(phone);
 		em.persist(cafe);
-		
-	
-	em.getTransaction().commit();
-	em.close();
+
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public void updaitCafe(EntityManagerFactory factory, Scanner sc) {
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		System.out.println("Select the id Cafe you want to change");
+		int id = sc.nextInt();
+		Cafe cafe = em.find(Cafe.class, id);
+		boolean isRun = true;
+		while (isRun) {
+			System.out.println("Select the field you want to change");
+			System.out.println("1. Address;");
+			System.out.println("2. Email;");
+			System.out.println("3. Full Description;");
+			System.out.println("4. Name;");
+			System.out.println("5. Phone;");
+			System.out.println("6. Type;");
+			System.out.println("7. Open hour;");
+			System.out.println("8. Clouse hour;");
+			System.out.println("0. to exit");
+			switch (sc.next()) {
+			case "1":
+				System.out.println("enter address: ");
+				String address = sc.next();
+				cafe.setAddress(address);
+				break;
+			case "2":
+				System.out.println("enter mail: ");
+				String email = sc.next();
+				cafe.setEmail(email);
+				break;
+			case "3":
+				System.out.println("enter fullDescription: ");
+				String fullDescription = sc.next();
+				cafe.setFullDescription(fullDescription);
+				break;
+			case "4":
+				System.out.println("enter name: ");
+				String name = sc.next();
+				cafe.setName(name);
+				break;
+			case "5":
+				System.out.println("enter phone: ");
+				String phone = sc.next();
+				cafe.setPhone(phone);
+				break;
+			case "6":
+				System.out.println("Choos type of cafe: ");
+				System.out.println("1. PUB;");
+				System.out.println("2. SUSHY;");
+				System.out.println("3. BAR;");
+				System.out.println("4. CAFE;");
+				System.out.println("5. RESTAURANT;");
+				switch (sc.next()) {
+				case "1":
+					cafe.setType(Type.PUB);
+					break;
+				case "2":
+					cafe.setType(Type.SUSHY);
+					break;
+				case "3":
+					cafe.setType(Type.BAR);
+					break;
+				case "4":
+					cafe.setType(Type.CAFE);
+					break;
+				case "5":
+					cafe.setType(Type.RESTAURANT);
+					break;
+				default:
+					break;
+				}
+				break;
+			case "7":
+				System.out.println("Enter id of open time");
+				int ot = sc.nextInt();
+				OpenClose open = em.find(OpenClose.class, ot);
+				cafe.setOpen(open);
+				break;
+			case "8":
+				System.out.println("Enter id of open time");
+				int ct = sc.nextInt();
+				OpenClose close = em.find(OpenClose.class, ct);
+				cafe.setClose(close);
+				break;
+			case "0":
+				isRun = false;
+				break;
+
+			default:
+				break;
+			}
+		}
+		em.persist(cafe);
+		em.getTransaction().commit();
+		em.close();
+
 	}
 	
 	
-	
+	public void removeCafe(EntityManagerFactory factory, Scanner sc){
+   EntityManager em = factory.createEntityManager();
+   em.getTransaction().begin();
+   System.out.println("Enter id Cafe if you wont delet: ");
+   int id = sc.nextInt();
+    Cafe cafe = em.find(Cafe.class, id);
+    em.remove(cafe);
+   em.getTransaction().commit();
+   em.close();
+		
+		
+		
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
--- JAVA_ADVANCED_PROJECT/src/main/java/kum/controler/Main.java
+++ JAVA_ADVANCED_PROJECT/src/main/java/kum/controler/Main.java
@@ -1,41 +1,12 @@
 package kum.controler;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
-import kum.entity.Cafe;
-import kum.entity.OpenClose;
-import kum.entity.Type;
 
 public class Main {
 
 	public static void main(String[] args) {
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("primary");
-//		EntityManager em = factory.createEntityManager();
-//		em.getTransaction().begin();
-////		OpenClose open = new OpenClose(LocalTime.of(10, 0));
-////		em.persist(open);
-////		em.detach(open);
-////		open.setTime(LocalTime.of(23, 0));
-////		em.merge(open);
-//		OpenClose open = em.find(OpenClose.class, 1);
-//		OpenClose close = em.find(OpenClose.class, 4);
-////		openClose.setTime(LocalTime.of(10, 0));
-////		em.remove(open);
-//		Cafe cafe = new Cafe();
-//		cafe.setAddress("Lviv, sq. Shevchenka 25");
-//		cafe.setClose(close);
-//		cafe.setEmail("Chocolad@gmail.com");
-//		cafe.setFullDescription("Full desc");
-//		cafe.setName("KUM_CAFE");
-//		cafe.setOpen(open);
-//		cafe.setPhone("+358956454");
-//		cafe.setShortDescription("Short desc");
-//		cafe.setType(Type.CAFE);
-//		em.persist(cafe);
-//		em.getTransaction().commit();
-//		em.close();
 		new Aplication().run();
 		factory.close();
 
